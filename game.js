982479360bd82824018f1af4a55a6946abdd26c1
SubhamShaww <64988887+SubhamShaww@users.noreply.github.com>
2020-07-12 19:23:54 +0530

    Update game.js

--- game.js
+++ game.js
@@ -61,7 +61,7 @@ function checkAnswer(currentLevel){
             $("body").removeClass("game-over");
         }, 200);
 
-        $("#level-title").text("Game Over, Press Any Key to Restart");
+        $("#level-title").text("Game Over, Press a Key to Restart");
         startOver();
 
     }
